bebd9b5263e4546b77555d07e5ea37b56e3f7095
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2020-09-30 19:24:06 +0300

    writing functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) return 0
+  else {
+      let min = array[0]
+      array.forEach(el => {
+        if(min > el) {
+          min = el
+        }
+      })
+      return min
+    }
+  }
+
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  else {
+      let max = array[0]
+      array.forEach(el => {
+        if(max < el) {
+          max = el
+        }
+      })
+      return max
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  else {
+      let sr = 0
+      array.forEach(el => {
+        sr += el
+      })
+      return sr / array.length
+    }
 }
